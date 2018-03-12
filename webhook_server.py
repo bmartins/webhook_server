7b61f347b5f9f4c5ad9a2a47549ac35d50221b00
Bruno Martins <bscmartins@gmail.com>
2018-03-12 11:04:38 +0000

    catch all url print url

--- webhook_server.py
+++ webhook_server.py
@@ -2,14 +2,24 @@ from bottle import route, run, request, Bottle
 
 app = application = Bottle()
 
-@app.route('/' , method=['POST'])
-def index():
+@app.route("/<url:re:.*>" , method=['POST'])
+def index(url):
     body = request.body.read()
+
+    query = ''
+    for k in request.query:
+        if query  != '':
+            query +=  '&'
+        query  += k + '=' + request.query[k]
+
+    if query != '':
+        query = '?' + query
     print "########################################################"
+    print "url: /" + url + query
     print body
     print "########################################################"
     return {"ok": 1}
 
 
-#run(host='0.0.0.0', port=8001)
+#app.run(host='0.0.0.0', port=8001)
 
